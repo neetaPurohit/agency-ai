8a3415d048ccaf1953fd4e4df932028c0116c5a3
neeta <neetapurohit56@gmail.com>
2026-08-08 13:36:59 +0530

    TrustedBy.jsx created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero";
+import TrustedBy from "./components/TrustedBy";
 
 const App = () => {
     const [theme, setTheme] = useState(localStorage.getItem('theme') ?  localStorage.getItem('theme') : 'light');
@@ -8,8 +9,9 @@ const App = () => {
     <div className="dark:bg-black relative">
         <Navbar theme={theme} setTheme={setTheme}/>
          <Hero/>
+         <TrustedBy/>
     </div>
-    
+
   )
 }
 
